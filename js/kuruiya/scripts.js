76ff240bd608ec221b96327f9af0e9d94e54831f
kuruiya <chindanai7715h@gmail.com>
2025-02-07 22:17:59 +0700

    Update scripts.js

--- js/kuruiya/scripts.js
+++ js/kuruiya/scripts.js
@@ -2,16 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const cards = document.querySelectorAll(".card");
     
     cards.forEach(card => {
-        let clickCount = 0;
-        
         card.addEventListener("click", function() {
-            clickCount++;
-            
-            if (clickCount === 1) {
-                card.classList.add("card-clicked");
-                setTimeout(() => clickCount = 0, 500); // รีเซ็ตคลิกหลังจาก 500 มิลลิวินาที
-            } else if (clickCount === 2) {
+            if (card.classList.contains("active")) {
                 window.location.href = card.querySelector("a").href;
+            } else {
+                cards.forEach(c => c.classList.remove("active"));
+                card.classList.add("active");
             }
         });
     });
