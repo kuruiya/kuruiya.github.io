029013861f38bda803318dd6c2c2780c123a31a4
kuruiya <chindanai7715h@gmail.com>
2025-02-07 21:43:56 +0700

    Update scripts.js

--- js/kuruiya/scripts.js
+++ js/kuruiya/scripts.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     cards.forEach(card => {
         card.addEventListener("click", function(event) {
+            if (event.target.tagName === 'A') {
+                return; // ไม่ทำอะไรถ้าคลิกที่ลิงก์
+            }
+
             if (card.classList.contains("expanded")) {
                 window.location.href = card.getAttribute("data-link");
             } else {
