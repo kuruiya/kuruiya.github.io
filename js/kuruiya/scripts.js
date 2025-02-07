abf83787c799afaff7a05516285900fddc79b393
kuruiya <chindanai7715h@gmail.com>
2025-02-07 22:24:22 +0700

    Update scripts.js

--- js/kuruiya/scripts.js
+++ js/kuruiya/scripts.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const cards = document.querySelectorAll(".card");
+    const bookCountElement = document.getElementById("book-count");
+    
+    // แสดงจำนวนหนังสือ
+    bookCountElement.textContent = `จำนวนหนังสือ: ${cards.length}`;
     
     cards.forEach(card => {
         card.addEventListener("click", function() {
